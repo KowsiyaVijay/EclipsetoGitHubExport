bc0d0e84c2efd0f2900abf79db12e6e72ab1d7cb
User <User@DESKTOP-HB5P3Q0>
2021-04-25 16:04:39 +0530

    Initial commit

---
+++ SeleniumTraining/src/DataTypesInJava.java
@@ -0,0 +1,53 @@
+
+public class DataTypesInJava {
+	
+	public static boolean b;
+	
+	public static byte a;
+	public static short c;
+	public static int s;
+	public static int _3S;
+	public static int String = 4;
+	public static int string = 67;
+
+
+
+	public static long w;
+	public static float o;
+	public static double j;
+	public static char z;
+
+public static String q;
+
+
+
+
+	public static void main(String[] args) {
+
+				
+		
+		System.out.println(String);
+		System.out.println(string);
+		/**System.out.println(c);
+		System.out.println(s);
+		System.out.println(w);
+		System.out.println(o);
+		System.out.println(j);
+		System.out.println(z);
+		System.out.println(q); **/
+
+		
+		
+
+
+
+
+		
+		
+		
+		
+		
+	}
+
+	
+}
