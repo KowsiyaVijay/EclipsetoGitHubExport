40c05ba3d9b23494e6e20a18d3d1476502b566b9
KowsiyaVijay <82774037+KowsiyaVijay@users.noreply.github.com>
2021-09-19 17:25:21 +0530

    Update DataTypesInJava.java

--- SeleniumTraining/src/DataTypesInJava.java
+++ SeleniumTraining/src/DataTypesInJava.java
@@ -21,7 +21,7 @@ public static String q;
 
 	public static void main(String[] args) {
 		
-		System.out.println(String);
+		//System.out.println(String);
 		System.out.println(string);
 		System.out.println(c);
 		System.out.println(s);
