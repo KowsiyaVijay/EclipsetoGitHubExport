3357a14fec0b531614ded8a7d16400ea517d6e69
KowsiyaVijay <82774037+KowsiyaVijay@users.noreply.github.com>
2021-09-19 15:37:50 +0530

    Testing

--- SeleniumTraining/src/WebdriverDemo.java
+++ SeleniumTraining/src/WebdriverDemo.java
@@ -8,7 +8,7 @@ public class WebdriverDemo {
 WebDriver driver = new ChromeDriver();
 driver.get("http://www.google.com");
 		
-		System.out.println("Test GitHub commit to Jenkins");
+		//System.out.println("Test GitHub commit to Jenkins");
 
 	}
 
